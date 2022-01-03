07ce2248bf38226091983413c829de5919584b01
alecsandrobs <alecsandro.stolk@gmail.com>
2022-01-03 13:43:30 +0100

    :rocket: added form tests :rocket:

--- tests/pages/form.page.ts
+++ tests/pages/form.page.ts
@@ -9,6 +9,47 @@ export class FormPage {
         actor().tap(this.comboConsole)
         actor().tap(this.comboConsoleOptions(option))
     }
+
+    static seekbar = CommonPage.contentDesc('slid', 'on seekbar')
+    // static seekbar = locate('//android.widget.SeekBar').as('seekbar')
+
+    static calcDiff = (percentual: number) => {
+        let diff: number = 0
+        if(percentual >= 0 && percentual <= 2) diff = 75
+        if(percentual >= 3 && percentual <= 15) diff = 70
+        if(percentual >= 16 && percentual <= 25) diff = 60
+        if(percentual >= 26 && percentual <= 29) diff = 55
+        if(percentual >= 30 && percentual <= 35) diff = 50
+        if(percentual >= 36 && percentual <= 39) diff = 45
+        if(percentual >= 40 && percentual <= 45) diff = 40
+        if(percentual >= 46 && percentual <= 49) diff = 35
+        if(percentual >= 50 && percentual <= 55) diff = 30
+        if(percentual >= 56 && percentual <= 59) diff = 25
+        if(percentual >= 60 && percentual <= 65) diff = 20
+        if(percentual >= 66 && percentual <= 69) diff = 15
+        if(percentual >= 70 && percentual <= 75) diff = 10
+        if(percentual >= 76 && percentual <= 79) diff = 5
+        if(percentual >= 80 && percentual <= 85) diff = 0
+        if(percentual >= 86 && percentual <= 89) diff = -5
+        if(percentual >= 90 && percentual <= 95) diff = -10
+        if(percentual >= 96 && percentual <= 99) diff = -15
+        if(percentual === 100) diff = -20
+        return diff
+    }
+    
+    static tapOnSeekBarAt = async (percent: number) => {
+        const elementBoundingRect: any = await actor().grabElementBoundingRect(FormPage.seekbar)
+        await actor().say(`I tap on ${percent}% on seekbar`)
+            await actor().touchPerform([{
+                action: 'tap',
+                options: {
+                    x: ((elementBoundingRect.width*percent)/100)+this.calcDiff(percent),
+                    y: elementBoundingRect.y
+                },
+                as: `on ${percent}%`
+            }]);
+    }
+
     static checkbox = CommonPage.contentDesc('check', 'on checkbox')
     static isChecked = () => actor().grabAttributeFrom(this.checkbox, 'checked')
     static switch = CommonPage.contentDesc('switch', 'on switch')
@@ -44,24 +85,30 @@ export class FormPage {
     static previousMonth: any = CommonPage.contentDesc('Previous month', 'previous month')
     static nextMonth: any = CommonPage.contentDesc('Next month', 'next month')
     static currentMonth: any = CommonPage.resourceId('android:id/date_picker_header_date', 'current month')
+
+    static repeatClick = (element: any, times: number) => {
+        for(let i=0; i<times; i++){
+            actor().tap(element)
+        }
+    }
     
     static selectMonth = async (month: string) => {
         const monthInit = 'Feb'
         const monthText: string = await actor().grabTextFrom(this.currentMonth)    
         if(monthText.includes(monthInit)){
             switch (month){
-                case '1': repeatClick(this.previousMonth, 1); break;
+                case '1': this.repeatClick(this.previousMonth, 1); break;
                 case '2': break;
-                case '3': repeatClick(this.nextMonth, 1); break;
-                case '4': repeatClick(this.nextMonth, 2); break;
-                case '5': repeatClick(this.nextMonth, 3); break;
-                case '6': repeatClick(this.nextMonth, 4); break;
-                case '7': repeatClick(this.nextMonth, 5); break;
-                case '8': repeatClick(this.nextMonth, 6); break;
-                case '9': repeatClick(this.nextMonth, 7); break;
-                case '10': repeatClick(this.nextMonth, 8); break;
-                case '11': repeatClick(this.nextMonth, 9); break;
-                case '12': repeatClick(this.nextMonth, 10); break;
+                case '3': this.repeatClick(this.nextMonth, 1); break;
+                case '4': this.repeatClick(this.nextMonth, 2); break;
+                case '5': this.repeatClick(this.nextMonth, 3); break;
+                case '6': this.repeatClick(this.nextMonth, 4); break;
+                case '7': this.repeatClick(this.nextMonth, 5); break;
+                case '8': this.repeatClick(this.nextMonth, 6); break;
+                case '9': this.repeatClick(this.nextMonth, 7); break;
+                case '10': this.repeatClick(this.nextMonth, 8); break;
+                case '11': this.repeatClick(this.nextMonth, 9); break;
+                case '12': this.repeatClick(this.nextMonth, 10); break;
                 default: break;
             }
         } else {
@@ -79,10 +126,28 @@ export class FormPage {
         actor().tap('OK')
     }
 
-}
-
-const repeatClick = (element: any, times: number) => {
-    for(let i=0; i<times; i++){
-        actor().tap(element)
+    static getHoursFromTime = (time: string) => {
+        return Number.parseInt(time.split(':')[0]).toString()
     }
+    
+    static getMinutesFromTime = (time: string) => {
+        return Number.parseInt(time.split(':')[1]).toString()
+    }
+    
+    static getTimeDisplay = (time:string) => {
+        return `${this.getHoursFromTime(time)}:${this.getMinutesFromTime(time)}`
+    }
+    
+    static selectTime = (time: string) => {
+        const timeDefault = '06:00'
+        const hour = this.getHoursFromTime(time)
+        let min = this.getMinutesFromTime(time)
+        actor().tap(timeDefault)
+        actor().tap(CommonPage.contentDesc(hour, `on hour ${hour}`))
+        actor().tap(CommonPage.contentDesc(min, `on minute(s) ${min}`))
+        actor().tap('OK')
+    }
+    
+    static timeDisplayed = (time: string) => locate(`//android.widget.TextView[@text='${time}']`).as(`time with text ${time}`)
+
 }
--- tests/spec/form.feature.ts
+++ tests/spec/form.feature.ts
@@ -20,6 +20,13 @@ Scenario('Should select option in combo', () => {
     I.see(optionSelect, FormPage.comboConsoleSelected)
 })
 
+Scenario('Should interact with seekbar', async () => {
+    const percent: number = 77
+    await FormPage.tapOnSeekBarAt(percent)
+    await I.tap('SALVAR')
+    await I.seeElement(CommonPage.textView(`Slider: ${percent}`, 'on slider percent'))
+})
+
 Scenario('Should interact with checkbox', async () => {
     // I.dontSeeCheckboxIsChecked(FormPage.checkbox) ### It is not working ###
     I.assertEqual(await FormPage.isChecked(), 'false', 'that checkbox should be unchecked')
@@ -43,7 +50,14 @@ Scenario('Should interact with datepicker', async () => {
     await FormPage.selectDate(date)
     // await I.selectDate(date)
     I.seeElement(FormPage.dateField(date))
-}).tag('@wip')
+})
+
+Scenario('Should interact with time', async () => {
+    const time = '01:05'
+    const timeDisplay = FormPage.getTimeDisplay(time)
+    FormPage.selectTime(time)
+    I.seeElement(FormPage.timeDisplayed(timeDisplay))
+})
 
 Scenario('Filling form', () => {
     const name = 'John Joe Jones'
@@ -52,17 +66,35 @@ Scenario('Filling form', () => {
     I.fillField(FormPage.nameField, name)
     FormPage.selectConsoleOption(console)
     I.tap(FormPage.checkbox)
+    I.tap(FormPage.switch)
     I.tap('SALVAR')
 
     I.seeElement(CommonPage.textView(`Nome: ${name}`, 'registered name'))
     I.seeElement(CommonPage.textView(`Console: ${console.toLocaleLowerCase()}`, 'selected console'))
     I.seeElement(CommonPage.textView('Slider: 25', 'on slider percent'))
-    I.seeElement(CommonPage.textView('Switch: On', 'on switch option'))
+    I.seeElement(CommonPage.textView('Switch: Off', 'on switch option'))
     I.seeElement(CommonPage.textView('Checkbox: Marcado', 'of checkbox'))
     I.seeElement(CommonPage.textView('Data: 01/01/2000', 'registered date'))
     I.seeElement(CommonPage.textView('Hora: 06:00', 'registered time'))
-});
+})
 
-After(() => { })
+Scenario('Filling form with delay', () => {
+    const name = 'John Smith'
 
-// const isChecked = (element: any) => I.grabAttributeFrom(element, 'checked')
+    I.fillField(FormPage.nameField, name)
+    I.tap('SALVAR DEMORADO')
+
+    I.waitForElement(CommonPage.textView(`Nome: ${name}`, 'registered name'), 30)
+
+    I.seeElement(CommonPage.textView(`Nome: ${name}`, 'registered name'))
+    I.seeElement(CommonPage.textView('Console: xone', 'selected console'))
+    I.seeElement(CommonPage.textView('Slider: 25', 'on slider percent'))
+    I.seeElement(CommonPage.textView('Switch: On', 'on switch option'))
+    I.seeElement(CommonPage.textView('Checkbox: Desabilitado', 'of checkbox'))
+    I.seeElement(CommonPage.textView('Data: 01/01/2000', 'registered date'))
+    I.seeElement(CommonPage.textView('Hora: 06:00', 'registered time'))
+})
+
+After(() => {
+    I.saveScreenshot('seekbar.png')
+})
